1a21aa8f5916344333adbafcb9edee8419fbbb81
Skywalker Li <skywalkerli@MacBookPro.T-mobile.com>
2025-06-11 15:51:34 -0400

    added effects

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import type { Expense } from "./types/expense.ts";
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [expenses, setExpenses] = useState<Expense[]>([]);
+
+  useEffect(() => {
+    const stored = localStorage.getItem("expenses");
+    if (stored) setExpenses(JSON.parse(stored))
+
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
   return (
     <>
       <div>
